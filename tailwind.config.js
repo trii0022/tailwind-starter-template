c45d20dd30a636970da781715a1be60a8c9208e4
Triin Trempi <tri0022@stud.kea.dk>
2022-02-03 17:10:39 +0100

    added everything

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,19 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      eggwhite: "#E7E0C9",
+      blueish: "#C1CFC0",
+      darkerblue: "#6B7AA1",
+      darkestblue: "#11324D",
+      black: "#000000",
+      white: "#ffffff",
+      yellow: "#FFBD35",
+    },
+
+    screens: {
+      xs: "540px",
+    },
     extend: {},
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
